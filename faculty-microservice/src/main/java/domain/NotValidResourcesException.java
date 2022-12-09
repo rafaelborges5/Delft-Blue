fa9732fac21b07dcadd2699cba13ac8842a101f1
Jasper van Beusekom <j.c.vanbeusekom-1@student.tudelft.nl>
2022-12-09 18:18:27 +0100

    Added NotValidResourceException

---
+++ faculty-microservice/src/main/java/domain/NotValidResourcesException.java
@@ -0,0 +1,11 @@
+package domain;
+
+/**
+ * Exception to indicate the that a resource is invalid
+ * */
+public class NotValidResourcesException extends Exception {
+
+    public NotValidResourcesException(String message) {
+        super(message);
+    }
+}
