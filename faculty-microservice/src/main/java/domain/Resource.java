45a0908784eb425f8eb754345af6b13d5d5224f0
Jasper van Beusekom <j.c.vanbeusekom-1@student.tudelft.nl>
2022-12-11 18:22:24 +0100

    Refactor from feedback

--- faculty-microservice/src/main/java/domain/Resource.java
+++ faculty-microservice/src/main/java/domain/Resource.java
@@ -32,16 +32,28 @@ public class Resource {
      * @throws NotValidResourcesException - when the resource restrictions are not met.
      */
     public Resource(int cpu, int gpu, int memory) throws NotValidResourcesException {
+        checkResourceValidity(cpu, gpu, memory);
+
         this.cpu = cpu;
         this.gpu = gpu;
         this.memory = memory;
+    }
 
-        if (this.cpu < 0 || this.gpu < 0 || this.memory < 0) {
+    /**
+     * Checks the constraint for the resource class.
+     * Called in the constructor before making the object.
+     * @param cpu - cpu of resource
+     * @param gpu - gpu of resource
+     * @param memory - memory of resource
+     * @throws NotValidResourcesException - when either negative values are found or other constraints are broken
+     */
+    public void checkResourceValidity(int cpu, int gpu, int memory) throws NotValidResourcesException {
+        if (cpu < 0 || gpu < 0 || memory < 0) {
             throw new NotValidResourcesException("Resource cannot have negative values");
         }
 
         //The business logic for issue #23
-        if (this.cpu < this.memory || this.cpu < this.gpu) {
+        if (cpu < memory || cpu < gpu) {
             throw new NotValidResourcesException("The cpu resources should be equal to at least max(memory, gpu)");
         }
     }
