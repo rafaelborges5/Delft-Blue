8f71ecc7b37d7f983426f7a9b34951d5692726ed
Razvan Nistor <r.i.nistor@student.tudelft.nl>
2022-12-14 23:08:04 +0100

    feat: Make request persistent

---
+++ faculty-microservice/src/main/java/sem/faculty/domain/RequestRepository.java
@@ -0,0 +1,18 @@
+package sem.faculty.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * A DDD repository for querying and persisting user aggregate roots.
+ */
+@Repository
+public interface RequestRepository extends JpaRepository<Request, String> {
+    /**
+     * Find request by id.
+     */
+    Optional<Request> findByRequestId(long requestId);
+
+}
