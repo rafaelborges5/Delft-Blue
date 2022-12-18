290e59be5fe15f60c0f06aa0d68314884f7e6b98
Ogreanu Codrin <C.Ogreanu@student.tudelft.nl>
2022-12-18 10:23:35 +0200

    Added utility methods to Resource

--- resource-manager-microservice/src/main/java/nl/tudelft/sem/resource/manager/domain/Resource.java
+++ resource-manager-microservice/src/main/java/nl/tudelft/sem/resource/manager/domain/Resource.java
@@ -1,12 +1,11 @@
 package nl.tudelft.sem.resource.manager.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import java.awt.geom.RectangularShape;
+import java.util.Objects;
 
 /**
  * DDD Value Object representing a Node's resources.
@@ -26,4 +25,49 @@ public class Resource {
 
     @Column(name = "memory", nullable = false)
     private transient int memResources;
+
+    /**
+     * Utility method for adding 2 {@link Resource Resources}.
+     *
+     * @param r1 the first addend
+     * @param r2 the second addend
+     * @return a new {@link Resource} that results from the operation
+     */
+    public static Resource add(Resource r1, Resource r2) {
+        return new Resource(
+                r1.cpuResources + r2.cpuResources,
+                r1.gpuResources + r2.gpuResources,
+                r1.memResources + r2.memResources
+        );
+    }
+
+    /**
+     * Utility method for subtracting 2 {@link Resource Resources}.
+     *
+     * @param r1 the subtrahend
+     * @param r2 the minuend
+     * @return a new {@link Resource} that results from the operation
+     */
+    public static Resource sub(Resource r1, Resource r2) {
+        return new Resource(
+                r1.cpuResources - r2.cpuResources,
+                r1.gpuResources - r2.gpuResources,
+                r1.memResources - r2.memResources
+        );
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Resource)) return false;
+        Resource resource = (Resource) o;
+        return cpuResources == resource.cpuResources &&
+                gpuResources == resource.gpuResources &&
+                memResources == resource.memResources;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cpuResources, gpuResources, memResources);
+    }
 }
