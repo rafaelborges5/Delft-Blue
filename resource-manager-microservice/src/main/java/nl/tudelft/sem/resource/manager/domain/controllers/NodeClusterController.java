e79b9d6952dce9d8239299f560b38fba1b6405af
Rafael Borges <R.BragaMedeirosMotaBorges@student.tudelft.nl>
2022-12-20 11:41:42 +0100

    feat: Added NodeClusterController skeleton

---
+++ resource-manager-microservice/src/main/java/nl/tudelft/sem/resource/manager/domain/controllers/NodeClusterController.java
@@ -0,0 +1,10 @@
+package nl.tudelft.sem.resource.manager.domain.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NodeClusterController {
+
+
+    public NodeClusterController
+}
