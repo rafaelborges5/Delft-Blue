da6fa1c2822840918e0db809502592d9d5c2cf3b
Jasper van Beusekom <j.c.vanbeusekom-1@student.tudelft.nl>
2023-01-17 11:17:55 +0100

    MAke everything work again

--- faculty-microservice/src/main/java/sem/faculty/Application.java
+++ faculty-microservice/src/main/java/sem/faculty/Application.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication()
 @EnableJpaRepositories("sem.faculty.domain")
-@ComponentScan(basePackages = {"sem.commons"})
+@ComponentScan(basePackages = {"sem.commons", "sem.faculty"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
